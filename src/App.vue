235eeab420900499cbf82f1f69739bd749ba55d8
Rafael Ventura <raf4ael@gmail.com>
2021-05-19 21:33:43 -0300

    consertando font-family

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,18 @@
   <v-app>
     <v-main>
       <HelloWorld />
-      <Home msg="Welcome to Your Vue.js App" />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import Home from "./components/Home.vue";
 
 export default {
   name: "App",
 
   components: {
     HelloWorld,
-    Home,
   },
 
   data: () => ({
@@ -27,6 +24,6 @@ export default {
 <style>
 @font-face {
   font-family: "CaviarDreams";
-  src: local(../src/assets/fonts/LouisGeorgeCafe.ttf), url() format("truetype");
+  src: url("./assets/fonts/LouisGeorgeCafe.ttf") format("truetype");
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2></h2>
-  </div>
+  <v-container>
+    <h1>Voce entrou na home component</h1>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  name: "Home",
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h2 {
+h1 {
   margin: 40px;
   font-family: "CaviarDreams", Helvetica, Arial;
 }
